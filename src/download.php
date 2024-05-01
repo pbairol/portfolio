358f76775aab2618d9a12d49b3e35ca64639760d
pbairol <87720420+pbairol@users.noreply.github.com>
2024-05-01 11:39:39 -0400

    Delete src/download.php

--- src/download.php
+++
@@ -1,7 +0,0 @@
-<?php
-$file = 'portfolioResume.pdf';
-header('Content-Description: File Transfer');
-header('Content-Type: application/pdf');
-header('Content-Disposition: attachment; filename="'.basename($file).'"');
-readfile($file);
-?>
